1f90ce505fc09dcafbb10201fbc4c5f56c56187a
josher <josher01.lin@gmail.com>
2018-11-14 16:22:19 +0800

    initial setting to enable bootstrap4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require_tree .
