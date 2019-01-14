d016c66c1314b0d58d0c38720335c3d0e3ecd11f
josher <josher01.lin@gmail.com>
2019-01-14 11:46:14 +0800

    try to fix pg migration issue..

--- db/migrate/20181113071444_create_services.rb
+++ db/migrate/20181113071444_create_services.rb
@@ -1,7 +1,7 @@
 class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
-      t.references :seller, foreign_key: {to_table: :users}
+      t.references :seller, references: :users
       t.references :category, foreign_key: true
       t.string :title
       t.text :description
