26684c0345fd9f286317bd0f52239e862e383f80
josher <josher01.lin@gmail.com>
2019-01-14 11:35:01 +0800

    modify migration

--- db/migrate/20181113071444_create_services.rb
+++ db/migrate/20181113071444_create_services.rb
@@ -1,7 +1,7 @@
 class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
-      t.references :user, foreign_key: true
+      t.references :seller, foreign_key: {to_table: :users}
       t.references :category, foreign_key: true
       t.string :title
       t.text :description
