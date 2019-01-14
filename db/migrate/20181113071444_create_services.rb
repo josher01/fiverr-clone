c105236f9f388626a55b8fb0855a3bb3c33b73f2
josher <josher01.lin@gmail.com>
2019-01-14 15:35:35 +0800

    revise seller as user

--- db/migrate/20181113071444_create_services.rb
+++ db/migrate/20181113071444_create_services.rb
@@ -1,7 +1,7 @@
 class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
-      t.references :seller, references: :users
+      t.references :user, references: :users
       t.references :category, references: :categories
       t.string :title
       t.text :description
