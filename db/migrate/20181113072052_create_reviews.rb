60def22f109405b2b391ea90120ddcf61f2d923c
josher <josher01.lin@gmail.com>
2019-01-14 13:39:45 +0800

    debug pg migration issue-1

--- db/migrate/20181113072052_create_reviews.rb
+++ db/migrate/20181113072052_create_reviews.rb
@@ -3,8 +3,8 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.integer :star
       t.text :comment
-      t.references :user, references: :users, foreign_key: true
-      t.references :service, references: :services, foreign_key: true
+      t.references :user, references: :users
+      t.references :service, references: :services
 
       t.timestamps
     end
