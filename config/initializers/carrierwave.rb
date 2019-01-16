07ba24313fb0e6beb73f84c71332505a3e9b40d4
josher <josher01.lin@gmail.com>
2019-01-16 14:15:27 +0800

    set up required gems for uploading images to s3 via carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
-  ##config.storage    = :aws
-  ##config.aws_bucket = ENV.fetch('S3_BUCKET_NAME') # for AWS-side bucket access permissions config, see section below
-  ##config.aws_acl    = 'public-read'
+  config.storage    = :aws
+  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME') # for AWS-side bucket access permissions config, see section below
+  config.aws_acl    = 'public-read'
 
   # Optionally define an asset host for configurations that are fronted by a
   # content host, such as CloudFront.
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
   #config.asset_host_public = true
 
   # The maximum period for authenticated_urls is only 7 days.
-  #config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
   # Set custom options such as cache control to leverage browser caching
   ##config.aws_attributes = {
@@ -17,12 +17,12 @@ CarrierWave.configure do |config|
   ##  cache_control: 'max-age=604800'
   ##}
 
-  ##config.aws_credentials = {
-  ##  access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-  ##  secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-  ##  region:            ENV.fetch('AWS_REGION'), # Required
-  ##  stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
-  ##}
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region:            ENV.fetch('AWS_REGION'), # Required
+    stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
+  }
 
   # Optional: Signing of download urls, e.g. for serving private content through
   # CloudFront. Be sure you have the `cloudfront-signer` gem installed and
