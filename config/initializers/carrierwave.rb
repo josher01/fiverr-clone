be830575571e6d99752161394f334b5bb1b810d8
josher <josher01.lin@gmail.com>
2019-01-24 17:47:46 +0800

    update carrierwave setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,9 +20,9 @@ CarrierWave.configure do |config|
   ##}
 
   config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    region:            ENV.fetch('AWS_REGION'), # Required
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region:            ENV['AWS_REGION'], # Required
     stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
   }
 
